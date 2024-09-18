360147ab76d82feabcd6da1593b3eaceecf8b8fb
blackseriesC6 <blackseries.n63@gmail.com>
2024-09-18 14:02:29 +0500

    did first two tasks for now

--- SQL Deep Dive/3 Valued Logic Exercises/questions.sql
+++ SQL Deep Dive/3 Valued Logic Exercises/questions.sql
@@ -1,3 +1,4 @@
+-- Active: 1726645206669@@127.0.0.1@5432@Store
 
 /*
 * DB: Store
@@ -5,11 +6,67 @@
 * Question: adjust the following query to display the null values as "No Address"
 */
 
+SELECT 
+    customerid, 
+    firstname, 
+    lastname, 
+    COALESCE(address1, 'No Address') AS address1, 
+    COALESCE(address2, 'No Address') AS address2, 
+    city, 
+    state, 
+    zip, 
+    country, 
+    region, 
+    email, 
+    phone, 
+    creditcardtype, 
+    creditcard, 
+    creditcardexpiration, 
+    username, 
+    password, 
+    age, 
+    income, 
+    gender
+FROM customers;
+
+
+
+
+
+
+
+
+
 /*
 * DB: Store
 * Table: customers
 * Question: Fix the following query to apply proper 3VL
 */
+
+SELECT customerid,
+       firstname,
+       lastname,
+       address1,
+       address2,
+       city,
+       state,
+       zip,
+       country,
+       region,
+       email,
+       phone,
+       creditcardtype,
+       creditcard,
+       creditcardexpiration,
+       username,
+       password,
+       age,
+       income,
+       gender
+FROM customers
+WHERE (age IS NOT NULL AND age >= 18) 
+  AND (income IS NOT NULL AND income > 0)  
+  AND (gender IS NOT NULL);  
 
 
 /*
--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -1,18 +1,24 @@
+-- Active: 1726645206669@@127.0.0.1@5432@World
 -- BEFORE YOU START
 /*
  * What database should I use for these exercises?
  * Name: Employees
  */
--- 
-
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
 
 
+
+SELECT AVG(salary) as average_salary FROM salaries
+
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
 
+SELECT EXTRACT(YEAR FROM birth_date) AS birth_year
+FROM employees
+ORDER BY birth_date DESC
+LIMIT 1;
 
 -- BEFORE YOU START
 /*
@@ -21,8 +27,13 @@
  */
 -- 
 
+
 -- Question 1: How many towns are there in france?
 -- Table: Towns
+
+SELECT COUNT(*) AS number_of_towns
+FROM Towns;  -- Adjust this value based on how France is represented in your data
+
 
 
 -- BEFORE YOU START
@@ -35,10 +46,19 @@
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
 
+SELECT COUNT(DISTINCT language) AS number_of_official_languages
+FROM countrylanguage
+
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
 
+SELECT AVG(lifeexpectancy) AS average_life_expectancy
+FROM country;  
+
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
+
+SELECT AVG(population) AS average_population_in_the_netherlands
+FROM city
 
 
